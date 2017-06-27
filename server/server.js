96e680125f9fb2d7b77456f3b13f3caae393e9da
TomShetler <tshetler93@gmail.com>
2017-06-26 20:31:52 -0400

    Add route for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -49,6 +49,25 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+/* DELETE A TASK */
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+/* START THE SERVER */
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
